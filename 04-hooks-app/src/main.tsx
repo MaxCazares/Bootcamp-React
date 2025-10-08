3c6f2c40a584cbea068590221386f8d1d6a88153
Max Cazares <maxocazares@hotmail.com>
2025-10-07 22:57:08 -0600

    Agregamos la libreria de UI a Hooks App edicion 2025

--- 04-hooks-app/src/main.tsx
+++ 04-hooks-app/src/main.tsx
@@ -6,8 +6,8 @@ import './index.css';
 // import { TrafficLight } from './01-useState/TrafficLight';
 // import { TrafficLightWithHook } from './02-useEffect/TrafficLightWithHook';
 // import { PokemonPage } from './03-examples/PokemonPage';
-import { FocusScreen } from './04-useRef/FocusScreen';
-// import { TasksApp } from './05-useReducer/TaskApp';
+// import { FocusScreen } from './04-useRef/FocusScreen';
+import { TasksApp } from './05-useReducer/TaskApp';	
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
@@ -15,7 +15,7 @@ createRoot(document.getElementById('root')!).render(
 		{/* <TrafficLight/> */}
 		{/* <TrafficLightWithHook /> */}
 		{/* <PokemonPage/> */}
-		<FocusScreen/>
-		{/* <TasksApp/> */}
+		{/* <FocusScreen/> */}
+		<TasksApp/>
 	</StrictMode>
 );
